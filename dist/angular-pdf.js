c6c43802afa3f72745761d0f6babf8ace87c3895
Sveer <sveer.sveer@gmail.com>
2017-12-18 00:47:15 +0300

    Fix canvas-id
    Now you could add existing canvas with id="pdf-canvas" to render on template.

--- dist/angular-pdf.js
+++ dist/angular-pdf.js
@@ -135,8 +135,14 @@ var NgPdf = exports.NgPdf = ["$window", "$document", "$log", function NgPdf($win
       var pageFit = attrs.scale === 'page-fit';
       var limitHeight = attrs.limitcanvasheight === '1';
       var scale = attrs.scale > 0 ? attrs.scale : 1;
-      var canvas = $document[0].createElement('canvas');
-      initCanvas(element, canvas);
+      var canvas=null;
+	  if (document.getElementById('pdf-canvas') !== null) {
+		  canvas = document.getElementById('pdf-canvas');
+	  }
+	  else{
+		  canvas = $document[0].createElement('canvas');
+		   initCanvas(element, canvas);
+	  }
       var creds = attrs.usecredentials;
       debug = attrs.hasOwnProperty('debug') ? attrs.debug : false;
 
